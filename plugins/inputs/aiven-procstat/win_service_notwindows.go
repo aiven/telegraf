d7d0c989968a898cd44a7ad1eeecd1d4bdea2dba
Michael Hoffmann <michael.hoffmann@aiven.io>
2022-01-21 09:29:26 +0100

    aiven: aiven patches
    see AIVEN_CHANGES.md for details

---
+++ plugins/inputs/aiven-procstat/win_service_notwindows.go
@@ -0,0 +1,12 @@
+//go:build !windows
+// +build !windows
+
+package aiven_procstat
+
+import (
+	"fmt"
+)
+
+func queryPidWithWinServiceName(winServiceName string) (uint32, error) {
+	return 0, fmt.Errorf("os not support win_service option")
+}
